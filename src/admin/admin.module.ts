0350df38a335c446e4cf8b0c2c75935fe3ae662d
Amr Zain <amrzain121@gmail.com>
2026-05-15 07:22:17 +0300

    feat: modularize admin routing and clean up AppModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { UsersModule } from '../users/users.module';
+import { CountriesModule } from './countries/countries.module';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [DashboardModule, UsersModule],
+  imports: [
+    DashboardModule,
+    UsersModule,
+    CountriesModule,
+    CitiesModule,
+    RouterModule.register([
+      {
+        path: 'admin',
+        children: [
+          { path: '', module: DashboardModule },
+          { path: '', module: UsersModule },
+          { path: '', module: CountriesModule },
+          { path: '', module: CitiesModule },
+        ],
+      },
+    ]),
+  ],
 })
 export class AdminModule {}
